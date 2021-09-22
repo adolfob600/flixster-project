57fe15d5495b8b18966419f41a9efb2c1fce78f3
adolfob600 <87164114+adolfob600@users.noreply.github.com>
2021-09-21 23:59:47 -0400

    flix part 2

---
+++ flixster/MovieDetailsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MovieDetailsViewController.swift
+//  flixster
+//
+//  Created by Adolfo Bustos-Cuautle on 9/20/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+
+    var movie: [String:Any]!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)!
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        posterView.af.setImage(withURL: posterURL)
+        backdropView.af.setImage(withURL: backdropURL!)
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Adolfo Bustos-Cuautle on 9/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
